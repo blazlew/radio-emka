d6b6c82c4527afbe8f117a6e104748d83e73d699
Błażej Lewandowski <blazej.lew95@gmail.com>
2021-04-10 13:17:24 +0200

    Add radio playback

--- src/screens/RadioPlayer.tsx
+++ src/screens/RadioPlayer.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import {Pressable, StatusBar, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  GestureResponderEvent,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import Config from 'react-native-config';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Video from 'react-native-video';
 import Colors from '../config/constants/colors';
 import commonStyles from '../config/constants/commonStyles';
 
-const PlayPauseButton: React.FC = () => {
+interface PlayPauseButtonProps {
+  onPress: (event: GestureResponderEvent) => void;
+  paused: boolean;
+}
+
+const PlayPauseButton: React.FC<PlayPauseButtonProps> = ({
+  onPress,
+  children,
+}) => {
   return (
     <View style={styles.pPButtonOuter}>
-      <View style={styles.pPButtonMid}>
-        <Pressable
-          style={styles.pPButtonInner}
-          android_ripple={{
-            color: Colors.RIPPLE,
-            borderless: true,
-          }}></Pressable>
-      </View>
+      <Pressable
+        onPress={onPress}
+        style={styles.pPButtonInner}
+        android_ripple={{
+          color: Colors.RIPPLE,
+          borderless: true,
+        }}>
+        {children}
+      </Pressable>
     </View>
   );
 };
 
 const RadioPlayer: React.FC = () => {
+  const [paused, setPaused] = useState(true);
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar
@@ -27,7 +45,19 @@ const RadioPlayer: React.FC = () => {
         backgroundColor="transparent"
         barStyle="light-content"
       />
-      <PlayPauseButton />
+      <Video
+        audioOnly
+        playInBackground
+        paused={paused}
+        poster="https://upload.wikimedia.org/wikipedia/commons/e/ec/John_Dunsworth.jpg" // TODO: add song posters
+        source={{
+          uri: Config.ESKA_LODZ_URL,
+        }}
+        style={styles.poster}
+        onBuffer={console.log} // TODO: add spinner while buffering, handle offline mode
+        onError={console.log} // TODO: smth
+      />
+      <PlayPauseButton onPress={() => setPaused(!paused)} />
     </SafeAreaView>
   );
 };
@@ -36,32 +66,32 @@ const styles = StyleSheet.create({
   safeArea: {
     backgroundColor: Colors.PRIMARY,
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    paddingBottom: 100,
+    // paddingBottom: 100,
+  },
+  poster: {
+    alignSelf: 'stretch',
+    marginHorizontal: 30,
+    aspectRatio: 1,
+    borderWidth: 1,
   },
   pPButtonOuter: {
-    backgroundColor: Colors.TERTIARY,
-    width: 100,
-    aspectRatio: 1,
-    borderRadius: 100 / 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    ...commonStyles.buttonShadow,
-  },
-  pPButtonMid: {
     backgroundColor: Colors.SECONDARY,
     width: 96,
     aspectRatio: 1,
     borderRadius: 96 / 2,
+    borderWidth: 2,
+    borderColor: Colors.TERTIARY,
     justifyContent: 'center',
     alignItems: 'center',
+    ...commonStyles.buttonShadow,
   },
   pPButtonInner: {
     backgroundColor: Colors.PRIMARY,
-    width: 84,
+    width: 82,
     aspectRatio: 1,
-    borderRadius: 84 / 2,
+    borderRadius: 82 / 2,
     justifyContent: 'center',
     alignItems: 'center',
   },
